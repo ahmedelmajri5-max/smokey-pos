47a9c3860e333b40625efedc282745cd1f331f73
ahmedelmajri5-max <ahmedelmajri5@gmail.com>
2026-06-01 19:55:52 +0200

    إضافة إعدادات Firebase للنسخة الخارجية

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.SMOKEY_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA8Dvh-l4U3s8LdKlnhWyH9c91OziGN_FE",
+  authDomain: "restaurant-test-269e3.firebaseapp.com",
+  projectId: "restaurant-test-269e3",
+  storageBucket: "restaurant-test-269e3.firebasestorage.app",
+  messagingSenderId: "301369717216",
+  appId: "1:301369717216:web:b378ea41a4d94718ddfb85"
+};
